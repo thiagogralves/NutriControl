d69ad46a818ab687811167674118d08291d71718
thiagogralves <149974068+thiagogralves@users.noreply.github.com>
2026-01-05 22:59:59 -0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    // Isso garante que o process.env.API_KEY usado no código 
-    // seja substituído pela variável de ambiente do Vercel
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+    // Garante que o process.env.API_KEY seja substituído durante o build
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || '')
   }
 });
